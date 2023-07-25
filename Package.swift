97526e749f63ff43eab7a7108f117f794da8d617
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-07-25 20:25:45 +0800

    Bump swift-syntax version to 509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-07-10-a

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     dependencies: [
         // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git",
-                 from: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-06-17-a"),
+                 from: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-07-10-a"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
